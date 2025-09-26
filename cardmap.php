e8b096957724ecf09fa934d0dab226a86419eeb1
Abe Prangishvili <abeprangishvili0@gmail.com>
2025-09-26 13:49:01 +0400

    Bump plugin version to 1.4.7

--- cardmap.php
+++ cardmap.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Card Map Builder Pro
 Description: Draggable card maps with images, captions, links, connections, admin editor + settings, and frontend shortcode with zoom/pan/fullscreen.
-Version: 1.4.6
+Version: 1.4.7
 Author: Abe Prangishvili
 */
 
